26a1364c7a4912c13f9ddf9297464b0e1324f012
Shawn_W <64041139+ShawnW24@users.noreply.github.com>
2020-11-16 12:59:53 -0600

    Add files via upload

---
+++ Palindrome.js
@@ -0,0 +1,14 @@
+let palindrome = function (word) {
+    let len = word.length;
+    let start = word.substring(0, Math.floor(len/2)).toLowerCase();
+    let end = word.substring(len - Math.floor(len/2)).toLowerCase();
+    // console.log(start,end);
+    let flip = end.split('').reverse().join('');
+    return (start == flip);
+
+}
+
+console.log(palindrome('radar')); 
+console.log(palindrome('racecar'));
+console.log(palindrome('macaroni'));
+console.log(palindrome('banana'));
